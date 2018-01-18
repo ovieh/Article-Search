c0f343d1a6b2091fd8ca11ad3c9ef2feff93293a
Ovieh <ovieh.mosley@gmail.com>
2018-01-18 14:48:47 -0500

    completed controller

--- controllers/articlesController.js
+++ controllers/articlesController.js
@@ -1,1 +1,36 @@
 const db = require("../models");
+
+module.exports = {
+	findAll(req, res) {
+		db.Article
+			.find(req.query)
+			.sort({ date: -1 })
+			.then( dbModel => res.json(dbModel))
+			.catch(err => res.status(422).json(err));
+	},
+	findById(req, res) {
+		db.Article
+			.findById(req.params.id)
+			.then(dbModel => res.json(dbModel))
+			.catch(err => res.status(422).json(err));
+	},
+	create(req, res) {
+		db.Article
+			.create(req.body)
+			.then(dbModel => res.json(dbModel))
+			.catch(err => res.status(422).json(err));
+	},
+	update(req, res) {
+		db.Article
+			.findOneAndUpdate({ _id: req.params.id }, req.body)
+			.then( dbModel => res.json(dbModel))
+			.catch(err => res.status(422).json(err));
+	},
+	remove(req, res) {
+		db.Article
+			.findById({ _id: req.params.id })
+			.then( dbModel => dbModel.remove())
+			.then( dbModel => res.json(dbModel))
+			.catch(err => res.status(422).json(err));
+	}
+};
