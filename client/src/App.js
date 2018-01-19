e9d6009afe6eeb5f00f4e443800a3ec29bfb311b
Ovieh <ovieh.mosley@gmail.com>
2018-01-19 14:01:46 -0500

    added navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,18 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Nav from "./components/Nav";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+
+const App = () =>
+	<Router>
+		<div>
+			<Nav />
+			<Switch>
+			</Switch>
+		</div>
+	</Router>;
+	
+
 
 export default App;
