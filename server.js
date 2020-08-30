6bb553634f4249ddf8f4f8f578c56d5c5805c965
Ovieh Mosley <ovieh@intellisense.io>
2020-08-30 12:20:34 +0100

    cleanup

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const routes = require("./routes");
-const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
 const logger = require("morgan");
@@ -21,20 +20,12 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/nytreact", {
   useNewUrlParser: true
 });
 
-console.log("URI", process.env.MONGODB_URI);
-
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
 app.use(routes);
-
-// Send every request to the React app
-// Define any API routes before this runs
-// app.get("*", function(req, res) {
-//   res.sendFile(path.join(__dirname, "./client/build/index.html"));
-// });
 
 app.listen(PORT, function () {
   console.log(`🌎 ==> Server now on port ${PORT}!`);
